76c16cd016ee925c548703e5b649d812259b6205
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2023-12-04 00:12:26 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Statistics from './Statistics/Statistics.jsx';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions.jsx';
+import Section from './Section/Section.jsx';
+import Notification from './Notification/Notification.jsx';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback(this.state);
+    const positivePercantage = this.countPositiveFeedbackPercentage(this.state);
+
+    const buttons = Object.keys(this.state);
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={buttons}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercantage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+// Компонент FeedbackOptions обов'язково повинен отримувати два props options={} onLeaveFeedback={}.
+//  options це массив ключів iз state
+// В компоненті FeedbackOptions обов'язково використовуйте метод map для створення розмітки
+// При першому відкритті сторінки, коли немає ще відгуків, повідомлення "There is no feedback"
+// потрібно рендерити по умові в Арр.
