ad6b59bf76f076457a5cbb0d64e5c3a1aec187a6
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2023-12-04 23:25:47 +0200

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,55 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Statistics from './Statistics/Statistics.jsx';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions.jsx';
 import Section from './Section/Section.jsx';
 import Notification from './Notification/Notification.jsx';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = type => {
+    if (type === 'good') return setGood(good + 1);
+    if (type === 'neutral') return setNeutral(neutral + 1);
+    if (type === 'bad') return setBad(bad + 1);
   };
 
-  handleFeedback = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
     return Math.round((good / total) * 100) || 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback(this.state);
-    const positivePercantage = this.countPositiveFeedbackPercentage(this.state);
+  const total = countTotalFeedback();
+  const positivePercantage = countPositiveFeedbackPercentage();
 
-    const buttons = Object.keys(this.state);
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={buttons}
-            onLeaveFeedback={this.handleFeedback}
+  const buttons = Object.keys({
+    good,
+    neutral,
+    bad,
+  });
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={buttons} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercantage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercantage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-// Компонент FeedbackOptions обов'язково повинен отримувати два props options={} onLeaveFeedback={}.
-//  options це массив ключів iз state
-// В компоненті FeedbackOptions обов'язково використовуйте метод map для створення розмітки
-// При першому відкритті сторінки, коли немає ще відгуків, повідомлення "There is no feedback"
-// потрібно рендерити по умові в Арр.
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
